4daf29d392865294380251f31c55dcb4e8bd1446
littlephony <uladzislau.lukashevich@gmail.com>
2023-03-27 18:35:21 +0200

    pretty code

--- server/app.py
+++ server/app.py
@@ -9,16 +9,18 @@ from model import SmileClassificationNet
 
 app = Flask(__name__)
 model = SmileClassificationNet()
-model.load_state_dict(torch.load('../model/model.pt', map_location='cpu'))
+model.load_state_dict(torch.load("../model/model.pt", map_location="cpu"))
 
 
 def transform_image(image_bytes):
-    transform = transforms.Compose([
-        transforms.Resize(255),
-        transforms.CenterCrop(178),
-        transforms.Resize(64),
-        transforms.ToTensor(),
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize(255),
+            transforms.CenterCrop(178),
+            transforms.Resize(64),
+            transforms.ToTensor(),
+        ]
+    )
 
     image = Image.open(io.BytesIO(image_bytes))
 
@@ -29,22 +31,22 @@ def get_prediction(image_bytes):
     image_tensor = transform_image(image_bytes)
     proba = model(image_tensor)
     return proba
-    
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return redirect(url_for('/api/predict'))
+    return redirect(url_for("/api/predict"))
 
 
-@app.route('/api/predict', methods=['POST'])
+@app.route("/api/predict", methods=["POST"])
 def predict():
-    if request.method == 'POST':
-        file = request.files['file']
+    if request.method == "POST":
+        file = request.files["file"]
         image_bytes = file.read()
         output = get_prediction(image_bytes)
 
-        return jsonify({'smile': output.item() > 0.5,
-                        'proba': output.item()})
-    
-if __name__ == '__main__':
+        return jsonify({"smile": output.item() > 0.5, "proba": output.item()})
+
+
+if __name__ == "__main__":
     app.run()
--- server/model.py
+++ server/model.py
@@ -1,40 +1,28 @@
 import torch.nn as nn
 
+
 class SmileClassificationNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.classifier = nn.Sequential(
-            nn.Conv2d(
-                in_channels=3, out_channels=32,
-                kernel_size=3, padding=1
-            ),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(p=0.5),
-            nn.Conv2d(
-                in_channels=32, out_channels=64,
-                kernel_size=3, padding=1
-            ),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(p=0.5),
-            nn.Conv2d(
-                in_channels=64, out_channels=128,
-                kernel_size=3, padding=1
-            ),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(
-                in_channels=128, out_channels=256,
-                kernel_size=3, padding=1
-            ),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.AvgPool2d(kernel_size=8),
             nn.Flatten(),
             nn.Linear(256, 1),
-            nn.Sigmoid()
+            nn.Sigmoid(),
         )
-
 
     def forward(self, x):
         return self.classifier(x)
